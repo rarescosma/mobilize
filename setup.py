14db8d87ef88b9df76c091440f92e42912911575
Rareș Cosma <rares@getbetter.ro>
2024-09-24 11:28:54 +0300

    bump to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mobilize",
-    version="0.1.0",
+    version="0.1.1",
     description="http URL to epub + mobi converter",
     entry_points={
         "console_scripts": ["mobilize=mobilize.main:main"],
